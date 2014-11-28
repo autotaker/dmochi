3c50749a726b780349f6233c04b526090c7bc24e
autotaker <autotaker@gmail.com>
2014-11-28 18:49:34 +0900

    support Tuple and Let(Syntax)

--- Syntax.hs
+++ Syntax.hs
@@ -13,9 +13,11 @@ data Program = Program { definitions :: [Def]
 
 type Def = (Symbol,Term)
 type Symbol = String
-data Term = C Bool | V Symbol
-          | Lam [Symbol] Term
-          | App Term [Term]
+data Term = C Bool | V Symbol | T [Term]
+          | Lam Symbol Term
+          | Let Symbol Term Term
+          | Proj Int Term
+          | App Term Term
           | Term :+: Term 
           | If Term Term Term 
           | Fail Symbol | Omega Symbol deriving(Ord,Eq)
@@ -24,8 +26,11 @@ instance Show Term where
     show (C True) = "true"
     show (C False) = "false"
     show (V x) = x
-    show (Lam xs t) = "λ <"++concat (intersperse "," xs)++"> . "++show t
-    show (App t ts) = "("++show t++") "++"<"++concat (intersperse "," $ map show ts)++">" 
+    show (T xs) = "<"++concat (intersperse "," (map show xs)) ++ ">"
+    show (Lam x t) = "λ " ++ x ++ " . "++show t
+    show (Let x t1 t2) = unwords ["let",x,"=",show t1,"in",show t2]
+    show (Proj i t) = "#"++show i++ " " ++ "(" ++ show t ++ ")"
+    show (App t1 t2) = "("++show t1++") "++ show t2
     show (t1 :+: t2) = unwords ["("++show t1++")","<>",show t2]
     show (If t1 t2 t3) = unwords ["if",show t1,"then",show t2,"else",show t3]
     show (Fail s) = "Fail(" ++ s ++ ")"
@@ -38,10 +43,13 @@ symbols (Program defs t0) = nub $ toList $ execWriter doit where
     doit = mapM_ (\(f,t) -> push f >> go t) defs >> go t0
     go (C _) = return ()
     go (V x) = push x
+    go (T ts) = mapM_ go ts
     go (Fail x) = push x
     go (Omega x) = push x
-    go (Lam xs t) = mapM_ push xs >> go t
-    go (App t ts) = go t >> mapM_ go ts
+    go (Lam x t) = push x >> go t
+    go (Let x t1 t2) = push x >> go t1 >> go t2
+    go (Proj _ t) = go t
+    go (App t1 t2) = go t1 >> go t2
     go (t1 :+: t2) = go t1 >> go t2
     go (If t1 t2 t3) = go t1 >> go t2 >> go t3
 
@@ -49,17 +57,23 @@ freeVariables :: Term -> [Symbol]
 freeVariables = nub . toList . execWriter . flip runReaderT S.empty . go where
     push = tell . Q.singleton
     go (V x) = fmap (S.member x) ask >>= \b -> unless b $ push x
-    go (Lam xs t) = local (flip (foldr S.insert) xs) $ go t
-    go (App t ts) = go t >> mapM_ go ts
+    go (T ts) = mapM_ go ts
+    go (Proj _ t) = go t
+    go (Lam x t) = local (S.insert x) $ go t
+    go (Let x t1 t2) = go t1 >> local (S.insert x) (go t2)
+    go (App t1 t2) = go t1 >> go t2
     go (t1 :+: t2) = go t1 >> go t2
     go (If t1 t2 t3) = go t1 >> go t2 >> go t3
     go _ = return ()
 
-boundVariables :: Term -> [[Symbol]]
+boundVariables :: Term -> [Symbol]
 boundVariables = nub . toList . execWriter . go where
     push = tell . Q.singleton
-    go (Lam xs t) = push xs >> go t
-    go (App t ts) = go t >> mapM_ go ts
+    go (Lam x t) = push x >> go t
+    go (Let x t1 t2) = push x >> go t1 >> go t2
+    go (Proj _ t) = go t
+    go (App t1 t2) = go t1 >> go t2
+    go (T ts) = mapM_ go ts
     go (t1 :+: t2) = go t1 >> go t2
     go (If t1 t2 t3) = go t1 >> go t2 >> go t3
     go _ = return ()
