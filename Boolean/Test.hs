aa92d7bb24322cfddfa474ad781efe217f172c84
autotaker <autotaker@gmail.com>
2015-03-04 17:37:25 +0900

    modified test

--- Boolean/Test.hs
+++ Boolean/Test.hs
@@ -9,8 +9,7 @@ import Boolean.Syntax
 import Control.Monad.Except
 import qualified Data.Map as M
 
-
---test :: Program -> ExceptT String IO [TType]
+test :: Program -> ExceptT String IO [TType]
 test input = do
     (p,syms) <- ExceptT $ return $ alphaConversion input
     liftIO $ mapM print (definitions p)
@@ -20,14 +19,12 @@ test input = do
     let env = M.fromList [ (x,(s,t)) | (x,t) <- syms, let s = senv M.! x]
     let ((lbl,edges),env') = buildGraph env p
     let g@(x,_,y) = reduce1 lbl edges env'
-    {-
     liftIO $ putStrLn $ ppGraph (fmap (\t -> case t of
         Just x -> x
         Nothing -> V "") y) x
-        -}
     let l = saturate p g
     let go (x:y:_) | x == y = liftIO (printContext x) >> return x
-        go (x:xs) = {- liftIO (printContext x) >> -} go xs
+        go (x:xs) = liftIO (printContext x) >> go xs
         go _ = undefined
     c <- go l
     return $ saturateTerm (flowEnv c) (symEnv c) (mainTerm p)
