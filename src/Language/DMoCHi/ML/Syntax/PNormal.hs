9f41159af51ff6f202c4596ce0f399ccea3f2505
autotaker <autotaker@gmail.com>
2016-09-09 18:04:03 +0900

    add missing files

---
+++ src/Language/DMoCHi/ML/Syntax/PNormal.hs
@@ -0,0 +1,342 @@
+{-# LANGUAGE FlexibleContexts, BangPatterns #-}
+module Language.DMoCHi.ML.Syntax.PNormal( Program(..)
+                                        , Exp(..)
+                                        , Value(..)
+                                        , Op(..)
+                                        , LetValue(..)
+                                        , FunDef(..)
+                                        , size
+                                        , freeVariables
+                                        , normalize
+                                        , module Language.DMoCHi.ML.Syntax.Type
+                                        ) where
+import Control.Monad
+import Control.Monad.State
+import Language.DMoCHi.Common.Util
+import Language.DMoCHi.Common.Id
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.Char
+import Language.DMoCHi.ML.Syntax.Type
+import qualified Language.DMoCHi.ML.Syntax.Typed as Typed
+
+data Program = Program { functions :: [(Id,FunDef)] 
+                       , mainTerm  :: Exp }
+
+data Exp = Value Value
+         | Let Type Id LetValue Exp -- associated type is that of body exp
+         | Assume Type AValue Exp
+         | Fail Type
+         | Branch Type !Int Exp Exp deriving(Eq,Show)
+
+data Value = Atomic AValue | Fun FunDef | Pair Value Value deriving(Eq,Show)
+
+data AValue = Var Id
+            | CInt  Integer
+            | CBool Bool
+            | Op Op deriving(Eq,Show)
+
+data Op = OpAdd AValue AValue
+        | OpSub AValue AValue
+        | OpEq  AValue AValue
+        | OpLt  AValue AValue
+        | OpLte AValue AValue
+        | OpAnd AValue AValue
+        | OpOr  AValue AValue
+        | OpFst Type AValue
+        | OpSnd Type AValue
+        | OpNot AValue  deriving(Eq,Show)
+
+data LetValue = LValue AValue
+              | LApp Type !Int Id Value
+              | LExp !Int Exp 
+              | LRand
+              deriving(Eq,Show)
+
+data FunDef = FunDef { ident :: !Int,
+                       arg   :: Id,
+                       body  :: Exp }
+                       deriving(Show, Eq)
+
+
+instance HasType Exp where
+    getType (Value v) = getType v
+    getType (Let a _ _ _) = a
+    getType (Assume a _ _) = a
+    getType (Fail a) = a
+    getType (Branch a _ _ _) = a
+
+instance HasType LetValue where
+    getType (LValue v) = getType v
+    getType (LApp ty _ _ _) = ty
+    getType (LExp _ e) = getType e
+    getType LRand = TInt
+
+instance HasType AValue where
+    getType (Var x) = getType x
+    getType (CInt _) = TInt
+    getType (CBool _) = TBool
+    getType (Op op) = getType op
+    
+instance HasType Value where
+    getType (Atomic v) = getType v
+    getType (Fun fdef) = getType fdef
+    getType (Pair v1 v2) = TPair (getType v1) (getType v2)
+
+instance HasType Op where
+    getType (OpAdd _ _) = TInt
+    getType (OpSub _ _) = TInt
+    getType (OpEq  _ _) = TBool
+    getType (OpLt  _ _) = TBool
+    getType (OpLte _ _) = TBool
+    getType (OpAnd _ _) = TBool
+    getType (OpOr  _ _) = TBool
+    getType (OpNot _)   = TBool
+    getType (OpFst a _)   = a
+    getType (OpSnd a _)   = a
+
+instance HasType FunDef where
+    getType e = TFun (getType (arg e)) (getType (body e))
+
+{-
+substV :: Id -> Value -> Value -> Value
+substV x v = go where
+    go (Var y) | name x == name y = v
+    go (Op op) = Op $ case op of
+        OpAdd a b -> OpAdd (go a) (go b)
+        OpSub a b -> OpSub (go a) (go b)
+        OpEq  a b -> OpEq  (go a) (go b)
+        OpLt  a b -> OpLt  (go a) (go b)
+        OpLte a b -> OpLte (go a) (go b)
+        OpAnd a b -> OpAnd (go a) (go b)
+        OpOr  a b -> OpOr  (go a) (go b)
+        OpNot a   -> OpNot (go a)
+        OpFst t a -> OpFst t (go a)
+        OpSnd t a -> OpSnd t (go a)
+    go (CInt i) = CInt i
+    go (CBool b) = CBool b
+    go (Pair v1 v2) = Pair (go v1) (go v2)
+
+evalV :: M.Map String Value -> Value -> Value
+evalV env = go where
+    go (Var y) = env M.! (name y)
+    go (Op op) = Op $ case op of
+        OpAdd a b -> OpAdd (go a) (go b)
+        OpSub a b -> OpSub (go a) (go b)
+        OpEq  a b -> OpEq  (go a) (go b)
+        OpLt  a b -> OpLt  (go a) (go b)
+        OpLte a b -> OpLte (go a) (go b)
+        OpAnd a b -> OpAnd (go a) (go b)
+        OpOr  a b -> OpOr  (go a) (go b)
+        OpNot a   -> OpNot (go a)
+        OpFst t a -> OpFst t (go a)
+        OpSnd t a -> OpSnd t (go a)
+    go (CInt i) = CInt i
+    go (CBool b) = CBool b
+    go (Pair v1 v2) = Pair (go v1) (go v2)
+    -}
+
+
+size :: Program -> Int
+size (Program fs t) = sum [ sizeE (body e) + 1 | (_,e) <- fs ] + sizeE t
+
+sizeE :: Exp -> Int
+sizeE (Value v)      = sizeV v
+sizeE (Let _ _ lv e)  = 1 + sizeLV lv + sizeE e
+sizeE (Assume _ v e) = 1 + sizeAV v + sizeE e
+sizeE (Fail _)       = 1
+sizeE (Branch _ _ e1 e2) = 1 + sizeE e1 + sizeE e2
+
+sizeAV :: AValue -> Int
+sizeAV (Var _) = 1
+sizeAV (CInt _) = 1
+sizeAV (CBool _) = 1
+-- sizeAV (Pair v1 v2) = 1 + sizeV v1 + sizeV v2
+sizeAV (Op op) = (case op of
+    OpAdd v1 v2 -> bin v1 v2 
+    OpSub v1 v2 -> bin v1 v2 
+    OpEq  v1 v2 -> bin v1 v2
+    OpLt  v1 v2 -> bin v1 v2
+    OpLte v1 v2 -> bin v1 v2
+    OpAnd v1 v2 -> bin v1 v2
+    OpOr  v1 v2 -> bin v1 v2
+    OpFst _ v1  -> unary v1
+    OpSnd _ v1  -> unary v1
+    OpNot v1    -> unary v1) where
+        bin v1 v2 = 1 + sizeAV v1 + sizeAV v2
+        unary v1 = 1 + sizeAV v1
+
+sizeV (Atomic v) = sizeAV v
+sizeV (Fun fdef) = 1 + sizeE (body fdef)
+sizeV (Pair v1 v2) = 1 + sizeV v1 + sizeV v2
+
+sizeLV :: LetValue -> Int
+sizeLV (LValue v) = sizeAV v
+sizeLV (LApp _ _ _ v) = 2 + sizeV v
+sizeLV (LExp _ e) = sizeE e
+sizeLV LRand = 1
+
+freeVariables :: S.Set Id -> Exp -> S.Set Id
+freeVariables = goE S.empty where
+    goE :: S.Set Id -> S.Set Id -> Exp -> S.Set Id
+    goE !acc env (Value v) = goV acc env v
+    goE !acc env (Let _ x lv e) = goE (goLV acc env lv) (S.insert x env) e
+    goE !acc env (Assume _ v e) = goE (goAV acc env v) env e
+    goE !acc _ (Fail _) = acc
+    goE !acc env (Branch _ _ e1 e2) = goE (goE acc env e1) env e2
+
+    goV :: S.Set Id -> S.Set Id -> Value -> S.Set Id
+    goV !acc env (Atomic v) = goAV acc env v
+    goV !acc env (Fun fdef) = goE acc (S.insert (arg fdef) env) (body fdef)
+    goV !acc env (Pair v1 v2) = goV (goV acc env v1) env v2
+
+    goAV :: S.Set Id -> S.Set Id -> AValue -> S.Set Id
+    goAV !acc env (Var x) = push acc env x
+    goAV !acc _ (CInt _) = acc
+    goAV !acc _ (CBool _) = acc
+    goAV !acc env (Op (OpAdd v1 v2)) = goAV (goAV acc env v1) env v2
+    goAV !acc env (Op (OpSub v1 v2)) = goAV (goAV acc env v1) env v2
+    goAV !acc env (Op (OpEq v1 v2)) = goAV (goAV acc env v1) env v2
+    goAV !acc env (Op (OpLt v1 v2)) = goAV (goAV acc env v1) env v2
+    goAV !acc env (Op (OpLte v1 v2)) = goAV (goAV acc env v1) env v2
+    goAV !acc env (Op (OpAnd v1 v2)) = goAV (goAV acc env v1) env v2
+    goAV !acc env (Op (OpOr v1 v2)) = goAV (goAV acc env v1) env v2
+    goAV !acc env (Op (OpFst _ v)) = goAV acc env v
+    goAV !acc env (Op (OpSnd _ v)) = goAV acc env v
+    goAV !acc env (Op (OpNot v)) = goAV acc env v
+    goLV !acc env (LValue v) = goAV acc env v
+    goLV !acc env (LApp _ _ f v) = goV (push acc env f) env v
+    goLV !acc env (LExp _ e) = goE acc env e
+    goLV !acc env LRand = acc
+    push acc env x | S.member x env = acc
+                   | otherwise = S.insert x acc
+
+alpha :: MonadId m => Value -> m Value
+alpha = alphaV M.empty where
+    alphaV :: MonadId m => M.Map Id Id -> Value -> m Value
+    alphaV env (Atomic av) = pure $ Atomic (renameA env av)
+    alphaV env (Fun (FunDef l x e)) = do
+        l' <- freshInt
+        x' <- alphaId x
+        e' <- alphaE (M.insert x x' env) e
+        return (Fun (FunDef l' x' e'))
+    alphaV env (Pair v1 v2) = Pair <$> alphaV env v1 <*> alphaV env v2
+
+    alphaE :: MonadId m => M.Map Id Id -> Exp -> m Exp
+    alphaE env (Value v) = Value <$> alphaV env v
+    alphaE env (Let ty x lv e) = do
+        x' <- alphaId x
+        Let ty x' <$> alphaLV env lv <*> alphaE (M.insert x x' env) e
+    alphaE env (Assume ty av e) = 
+        Assume ty (renameA env av) <$> alphaE env e
+    alphaE _ (Fail ty) = pure (Fail ty)
+    alphaE env (Branch ty l e1 e2) = do
+        l' <- freshInt
+        Branch ty l' <$> alphaE env e1 <*> alphaE env e2
+
+    alphaLV :: MonadId m => M.Map Id Id -> LetValue -> m LetValue
+    alphaLV env (LValue av) = pure $ LValue (renameA env av)
+    alphaLV env (LApp ty l f v) = 
+        freshInt >>= (\l' -> LApp ty l' (rename env f) <$> (alphaV env v))
+    alphaLV env (LExp l e) = 
+        freshInt >>= (\l' -> LExp l' <$> alphaE env e)
+    alphaLV env (LRand) = pure LRand
+
+    renameA env (Var x) = Var (rename env x)
+    renameA env (CInt i) = CInt i
+    renameA env (CBool b) = CBool b
+    renameA env (Op op) = Op $ case op of
+        OpAdd v1 v2 -> OpAdd (renameA env v1) (renameA env v2)
+        OpSub v1 v2 -> OpSub (renameA env v1) (renameA env v2)
+        OpEq v1 v2 -> OpEq (renameA env v1) (renameA env v2)
+        OpLt v1 v2 -> OpLt (renameA env v1) (renameA env v2)
+        OpLte v1 v2 -> OpLte (renameA env v1) (renameA env v2)
+        OpAnd v1 v2 -> OpAnd (renameA env v1) (renameA env v2)
+        OpOr v1 v2 -> OpOr (renameA env v1) (renameA env v2)
+        OpFst ty v -> OpFst ty (renameA env v)
+        OpSnd ty v -> OpSnd ty (renameA env v)
+        OpNot v -> OpNot (renameA env v)
+
+    rename env x = case M.lookup x env of
+        Just y -> y
+        Nothing -> x
+
+
+alphaId :: MonadId m => Id -> m Id
+alphaId x = Id (getType x) <$> freshId (elim $ name x)
+    where
+    elim x = reverse $ dropWhile (=='_') $ dropWhile isDigit $ reverse x
+    
+
+normalize :: MonadId m => Typed.Program -> m Program
+normalize prog = do
+    let fs = Typed.functions prog
+        e0 = Typed.mainTerm prog
+    fs' <- forM fs $ \(f,fdef) -> (,) f <$> convertF M.empty fdef
+    e0' <- convertE M.empty e0
+    return $ Program fs' e0'
+    where
+    convertE :: MonadId m => M.Map Id Value -> Typed.Exp -> m Exp
+    convertE env (Typed.Value v) = Value <$> convertV env v
+    convertE env (Typed.Let ty x lv e) = case lv of
+        Typed.LValue v -> do
+            v' <- convertV env v
+            case v' of
+                Atomic av -> Let ty x (LValue av) <$> convertE env e
+                _ -> convertE (M.insert x v' env) e
+        Typed.LApp ty' l f v -> do
+            v' <- convertV env v
+            Let ty x (LApp ty' l f v') <$> convertE env e
+        Typed.LExp l e1 -> do
+            e1' <- convertE env e1
+            Let ty x (LExp l e1') <$> convertE env e
+        Typed.LRand -> do
+            Let ty x LRand <$> convertE env e
+    convertE env (Typed.Assume ty v e) = do
+        Atomic av <- convertV env v
+        Assume ty av <$> convertE env e
+    convertE env (Typed.Fail ty) = pure (Fail ty)
+    convertE env (Typed.Branch ty l e1 e2) =
+        Branch ty l <$> convertE env e1 <*> convertE env e2
+            
+    convertF :: MonadId m => M.Map Id Value -> Typed.FunDef -> m FunDef
+    convertF env (Typed.FunDef l x e) = FunDef l x <$> convertE env e
+
+    convertV :: MonadId m => M.Map Id Value -> Typed.Value -> m Value
+    convertV env (Typed.Var x) = 
+        case M.lookup x env of
+            Nothing -> pure (Atomic $ Var x)
+            Just v  -> alpha v
+    convertV env (Typed.CInt i) = pure (Atomic $ CInt i)
+    convertV env (Typed.CBool b) = pure (Atomic $ CBool b)
+    convertV env (Typed.Pair v1 v2) = 
+        Pair <$> convertV env v1 <*> convertV env v2
+    convertV env (Typed.Op op) = (case op of
+        Typed.OpAdd v1 v2 -> bin OpAdd v1 v2
+        Typed.OpSub v1 v2 -> bin OpSub v1 v2
+        Typed.OpEq v1 v2 -> bin OpEq v1 v2
+        Typed.OpLt v1 v2 -> bin OpLt v1 v2
+        Typed.OpLte v1 v2 -> bin OpLte v1 v2
+        Typed.OpAnd v1 v2 -> bin OpAnd v1 v2
+        Typed.OpOr v1 v2 -> bin OpOr v1 v2
+        Typed.OpFst ty v -> do
+            v' <- convertV env v
+            case v' of
+                Atomic av -> return $ Atomic $ Op $ OpFst ty av
+                Pair v1 _ -> pure v1
+        Typed.OpSnd ty v -> do
+            v' <- convertV env v
+            case v' of
+                Atomic av -> return $ Atomic $ Op $ OpFst ty av
+                Pair _ v2 -> pure v2
+        Typed.OpNot v -> do
+            Atomic av <- convertV env v
+            return $ Atomic $ Op $ OpNot av)
+        where 
+        bin f v1 v2 = do
+            Atomic av1 <- convertV env v1
+            Atomic av2 <- convertV env v2
+            return $ Atomic $ Op (f av1 av2)
+
+            
+
---
+++ src/Language/DMoCHi/ML/Syntax/Type.hs
@@ -0,0 +1,42 @@
+module Language.DMoCHi.ML.Syntax.Type where
+import Language.DMoCHi.Common.Util
+import Text.PrettyPrint
+
+data Id = Id { _type :: Type, name :: String } deriving(Show)
+
+instance Eq Id where
+    (==) = (==) `on` name
+
+instance Ord Id where
+    compare = compare `on` name
+
+data Type = TInt | TBool | TPair Type Type | TFun Type Type deriving(Eq)
+
+class HasType m where
+    getType :: m -> Type
+
+instance HasType Id where
+    getType = _type
+
+pprintT :: Int -> Type -> Doc
+pprintT _ TInt = text "int"
+pprintT _ TBool = text "bool"
+pprintT assoc (TPair t1 t2) =
+    let d1 = pprintT 1 t1
+        d2 = pprintT 1 t2
+        d  = d1 <+> text "*" <+> d2
+    in if assoc <= 0 then d else parens d
+pprintT assoc (TFun t1 t2) =
+    let d1 = pprintT 1 t1
+        d2 = pprintT 0 t2
+        d  = d1 <+> text "->" <+> d2
+    in if assoc == 0 then d else parens d
+
+instance Show Type where
+    show = render . pprintT 0
+
+orderT :: Type -> Int
+orderT TInt = 0
+orderT TBool = 0
+orderT (TPair t1 t2) = max (orderT t1) (orderT t2)
+orderT (TFun t1 t2)  = max (orderT t1+1) (orderT t2)
