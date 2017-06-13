884733f1cf2b1e3f91011cc3aa223d727cb8dd72
autotaker <autotaker@gmail.com>
2017-06-13 18:40:36 +0900

    delete hccs

--- Setup.hs
+++ Setup.hs
@@ -17,28 +17,5 @@ import System.Exit
 import System.FilePath((</>))
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks{ postBuild = postBuildHook
-                                           , postCopy  = postCopyHook
-                                           , postClean = postCleanHook }
+main = defaultMain
 
-postBuildHook :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
-postBuildHook args flags pkg_descr lbi = do
-    let dir = dataDir pkg_descr
-    -- make -C hccs
-    rawSystemExit (fromFlag (buildVerbosity flags)) "make" ["-C",dir]
-    return ()
-
-postCopyHook :: Args -> CopyFlags -> PackageDescription -> LocalBuildInfo -> IO ()
-postCopyHook args flags pkg_descr lbi = do
-    let copydest = fromFlag (copyDest flags)
-    let destData = datadir $ absoluteInstallDirs pkg_descr lbi copydest
-    let target = destData </> "hcsolver"
-    -- chmod a+x $DEST_DATA_DIR/hcsolver
-    rawSystemExit (fromFlag (copyVerbosity flags)) "chmod" ["a+x",target]
-
-postCleanHook :: Args -> CleanFlags -> PackageDescription -> () -> IO ()
-postCleanHook arg flags pkg_descr _ = do
-    let dir = dataDir pkg_descr
-    -- make -C hccs clean
-    rawSystemExit (fromFlag (cleanVerbosity flags)) "make" ["-C",dir, "clean"]
-    
